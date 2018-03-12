b973ff6a93f2c1286ca134aa7d76a55c71d34920
Python-x <2248177011@qq.com>
2018-03-12 11:48:53 +0800

    哈哈

--- 面向对象/plane_sprites.py
+++ 面向对象/plane_sprites.py
@@ -1,2 +1,25 @@
 import pygame
 SCREEN_RECT = pygame.Rect(0, 0, 480, 700)
+class PlaneGame(pygame.sprite.Sprite):
+	
+	def __init__(self,new_image,new_speed = 1):
+		super().__init__()
+		#图片  速度  位置
+		self.image = pygame.image.load(new_image)
+		self.speed = new_speed
+		#获取图片的宽和高
+		self.rect = self.image.get_rect()
+
+	def update(self):
+		self.rect.y += 1
+
+class Background(PlaneGame):
+	def __init__(self, new_image):
+		super().__init__(new_image)
+		
+	def update(self):
+		super().update()
+		if self.rect.y == SCREEN_RECT.height:
+			self.rect.y = 0
+
+		
---
+++ 面向对象/随机.py
@@ -0,0 +1,32 @@
+import random
+b = ["张燕娜","冯新民","李明瑞","韩俊杰"]
+c = ["潘志伟","陈怀勇","王喜亮","王琳","贾淼浩"]
+d = ["刘博超","胡宥嘉","原建波","于奇林","盖轩宾"]
+e = ["赵圆","刘婷婷","李志博","王润泽","王含青"]
+f = ["段金松","周润林","吴金航","刘博宇"]
+g = ["杜登进","门维甲","庞源松","崔树","安华峰"]
+int1 = random.randint(0,len(b)-1)
+print(b[int1])
+
+int2 = random.randint(0,len(c)-1)
+print(c[int2])
+
+int3 = random.randint(0,len(d)-1)
+print(d[int3])
+
+int4 = random.randint(0,len(e)-1)
+print(e[int4])
+
+int5 = random.randint(0,len(f)-1)
+print(f[int5])
+
+int6 = random.randint(0,len(g)-1)
+print(g[int6])
+
+
+
+
+
+
+
+   
